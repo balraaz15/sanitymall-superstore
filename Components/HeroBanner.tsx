d00ddac989cf3f8e048dc7b895ef24b9ee8994c2
balraaz15 <balraaz11@gmail.com>
2023-01-04 00:28:23 +0545

    add herobanner

--- Components/HeroBanner.tsx
+++ Components/HeroBanner.tsx
@@ -1,27 +1,30 @@
-import Image from 'next/image'
-import Link from 'next/link'
-import React from 'react'
-import { urlFor } from '../lib/client'
+import Link from "next/link";
+import React from "react";
+import { urlFor } from "../lib/client";
 
 const HeroBanner = ({ heroBanner }: any) => {
   return (
     <div className="hero-banner-container">
       <div>
-        <p className="beats-solo">
-          {heroBanner.smallText}
-        </p>
+        <p className="beats-solo">{heroBanner.smallText}</p>
         <h3>{heroBanner.midText}</h3>
         <h1>{heroBanner.discount}</h1>
-        <img src={urlFor(heroBanner.image)} alt="Camera" className="hero-banner-image" />
+        <img
+          src={urlFor(heroBanner.image)}
+          alt="Camera"
+          className="hero-banner-image"
+        />
         <div>
-          <Link href={`/product/${heroBanner.product}`}><button type="button">{heroBanner.buttonText}</button></Link>
+          <Link href={`/product/${heroBanner.product}`}>
+            <button type="button">{heroBanner.buttonText}</button>
+          </Link>
           <div className="desc">
             <h5>{heroBanner.desc}</h5>
           </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default HeroBanner
+export default HeroBanner;
