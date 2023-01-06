165d21cbfa9db957a371970dc92fcd3bc7c30fdc
balraaz15 <balraaz11@gmail.com>
2023-01-06 12:17:43 +0545

    improve formatting

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,19 +2,20 @@ import React from "react";
 import { Footer, FooterBanner, HeroBanner, Product } from "../Components";
 import { client } from "../lib/client";
 
-
 const Home = ({ products, bannerData }: any) => {
   return (
     <>
       <HeroBanner heroBanner={bannerData.length && bannerData[0]} />
-      {console.log(bannerData, products)}
+
       <div className="products-heading">
         <h2>Best Selling Products</h2>
         <p>Speakers of many variations</p>
       </div>
 
       <div className="products-container">
-        {products?.map((product: any) => <Product key={product?._id} product={product} />)}
+        {products?.map((product: any) => (
+          <Product key={product?._id} product={product} />
+        ))}
       </div>
 
       <FooterBanner footerBanner={bannerData.length && bannerData[0]} />
@@ -30,8 +31,8 @@ export const getServerSideProps = async () => {
   const bannerData = await client.fetch(bannerQuery);
 
   return {
-    props: {products, bannerData}
-  }
-}
+    props: { products, bannerData },
+  };
+};
 
 export default Home;
