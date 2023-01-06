ec348e97a8ee6338d5b05c8a699676c2f91cd261
balraaz15 <balraaz11@gmail.com>
2023-01-06 12:18:03 +0545

    add cart component

--- Components/Cart.tsx
+++ Components/Cart.tsx
@@ -1,9 +1,116 @@
-import React from 'react'
+import Link from "next/link";
+import React, { useRef } from "react";
+import {
+  AiOutlineLeft,
+  AiOutlineMinus,
+  AiOutlinePlus,
+  AiOutlineShopping,
+} from "react-icons/ai";
+import { TiDeleteOutline } from "react-icons/ti";
+import { useStateContext } from "../context/StateContext";
+import { urlFor } from "../lib/client";
+import ProductData from "../types/Product";
 
 const Cart = () => {
-  return (
-    <div>Cart</div>
-  )
-}
+  const cartRef = useRef();
+  const {
+    totalQuantities,
+    totalPrice,
+    cartItems,
+    setShowCart,
+    toggleCartItemQty,
+    removeFromCart,
+  }: any = useStateContext();
 
-export default Cart
+  return (
+    <div className="cart-wrapper" ref={cartRef}>
+      <div className="cart-container">
+        <button
+          type="button"
+          className="cart-heading"
+          onClick={() => setShowCart(false)}
+        >
+          <AiOutlineLeft />
+          <span className="heading">Your Cart</span>
+          <span className="cart-num-items">({totalQuantities} items)</span>
+        </button>
+
+        {cartItems.length < 1 && (
+          <div className="empty-cart">
+            <AiOutlineShopping size={150} />
+            <h3>Your Shopping Cart is Empty.</h3>
+            <Link href="/">
+              <button
+                type="button"
+                onClick={() => setShowCart(false)}
+                className="btn"
+              >
+                Continue Shopping
+              </button>
+            </Link>
+          </div>
+        )}
+
+        <div className="product-container">
+          {cartItems.length > 0 &&
+            cartItems.map((item: any) => (
+              <div className="product" key={item._id}>
+                <img
+                  src={urlFor(item?.image[0])}
+                  className="cart-product-image"
+                  alt={item.name}
+                />
+                <div className="item-desc">
+                  <div className="flex top">
+                    <h5>{item.name}</h5>
+                    <h4>${item.price * item.quantity}</h4>
+                  </div>
+                  <div className="flex bottom">
+                    <div>
+                      <p className="quantity-desc">
+                        <span
+                          className="minus"
+                          onClick={() => toggleCartItemQty(item?._id, "dec")}
+                        >
+                          <AiOutlineMinus />{" "}
+                        </span>
+                        <span className="num">{item.quantity}</span>
+                        <span
+                          className="plus"
+                          onClick={() => toggleCartItemQty(item?._id, "inc")}
+                        >
+                          <AiOutlinePlus />{" "}
+                        </span>
+                      </p>
+                    </div>
+                    <button
+                      type="button"
+                      className="remove-item"
+                      onClick={() => removeFromCart(item)}
+                    >
+                      <TiDeleteOutline />
+                    </button>
+                  </div>
+                </div>
+              </div>
+            ))}
+        </div>
+        {cartItems.length > 0 && (
+          <div className="cart-bottom">
+            <div className="total">
+              <h3>Subtotal: </h3>
+              <h3>${totalPrice}</h3>
+            </div>
+            <div className="btn-container">
+              <button className="btn" type="button">
+                Checkout
+              </button>
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Cart;
