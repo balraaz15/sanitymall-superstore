3026ffd37f17423de1dd4042c3a4594fa37c78ee
balraaz15 <balraaz11@gmail.com>
2023-01-06 12:14:42 +0545

    add product card component

--- Components/Product.tsx
+++ Components/Product.tsx
@@ -1,21 +1,25 @@
-import Image from 'next/image'
-import Link from 'next/link'
-import React from 'react'
-import { urlFor } from '../lib/client'
+import Link from "next/link";
+import React from "react";
+import { urlFor } from "../lib/client";
 
-const Product = ({ product: {image, name, slug, price} }: any) => {
-  console.log("Image: ", image);
+const Product = ({ product: { image, name, slug, price } }: any) => {
   return (
     <div>
       <Link href={`/product/${slug.current}`}>
         <div className="product-card">
-          <img src={urlFor(image.length && image[0])} alt={name} width={250} height={250} className="product-image" />
+          <img
+            src={urlFor(image.length && image[0])}
+            alt={name}
+            width={250}
+            height={250}
+            className="product-image"
+          />
           <p className="product-name">{name}</p>
           <p className="product-price">${price}</p>
         </div>
       </Link>
     </div>
-  )
-}
+  );
+};
 
-export default Product
+export default Product;
