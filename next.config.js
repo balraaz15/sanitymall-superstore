94d5b5eaa3b0d48705a40574e60e7804201ad3d0
balraaz15 <balraaz11@gmail.com>
2023-01-09 22:22:17 +0545

    NL: ignore build errors while building in vercel

--- next.config.js
+++ next.config.js
@@ -5,6 +5,11 @@ const nextConfig = {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
     ignoreDuringBuilds: true,
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
   },
 }
 
