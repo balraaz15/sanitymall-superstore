17a8b40277ade4653ceb8365cbfdf4b5eb9624b6
balraaz15 <balraaz11@gmail.com>
2023-01-09 22:31:44 +0545

    removing invalid config from next config

--- next.config.js
+++ next.config.js
@@ -5,11 +5,6 @@ const nextConfig = {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
     ignoreDuringBuilds: true,
-    // !! WARN !!
-    // Dangerously allow production builds to successfully complete even if
-    // your project has type errors.
-    // !! WARN !!
-    ignoreBuildErrors: true,
   },
 }
 
